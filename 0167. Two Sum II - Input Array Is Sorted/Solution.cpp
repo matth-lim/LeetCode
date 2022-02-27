547bd71d14c9a8bc35dfdb7a1fb032eb6264bc5e
Solitude <solitude888888@gmail.com>
2022-02-27 23:46:17 +0800

    init question 167

---
+++ 0167. Two Sum II - Input Array Is Sorted/Solution.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size() - 1;
+        while (l < r)
+        {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target)
+            {
+                return{l + 1, r + 1};
+            }
+            else if (sum < target)
+                l++;
+            else
+                r--;
+        }
+        return {};
+    }
+};
