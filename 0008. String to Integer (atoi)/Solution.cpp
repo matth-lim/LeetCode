f54c5ac6c1278e2bb02c46a1b67aab238b825be4
Solitude <solitude888888@gmail.com>
2022-02-27 19:46:14 +0800

    update cpp solution

--- 0008. String to Integer (atoi)/Solution.cpp
+++ 0008. String to Integer (atoi)/Solution.cpp
@@ -25,10 +25,11 @@ public:
         while (index < s.size() && s[index] >= '0' && s[index] <= '9')
         {
             int digit = s[index] - '0';
-            if (isnegative == false && (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)))
-                return INT_MAX;
-            if (isnegative && (-result < INT_MIN / 10 || (-result == INT_MIN / 10 && -digit <= INT_MIN % 10)))
-                return INT_MIN;
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
+                if (isnegative)
+                    return INT_MIN;
+                else
+                    return INT_MAX;
             result = result * 10 + digit;
             index++;
         }
