aa7201fe49df22d756acc1219f63120aec608408
Solitude <solitude888888@gmail.com>
2022-02-28 17:57:43 +0800

    update all solution

--- 0026. Remove Duplicates from Sorted Array/Solution.cpp
+++ 0026. Remove Duplicates from Sorted Array/Solution.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int l = 0;
-        for (int i = 1; i < nums.size(); i++)
+        for (int i = l + 1; i < nums.size(); i++)
         {
             if (nums[l] != nums[i])
             {
--- 0026. Remove Duplicates from Sorted Array/Solution.py
+++ 0026. Remove Duplicates from Sorted Array/Solution.py
@@ -5,7 +5,7 @@ class Solution:
         rtype: int
         """
         l = 0
-        for x in range(len(nums)):
+        for x in range(l + 1, len(nums)):
             if nums[x] != nums[l]:
                 l += 1
                 nums[l] = nums[x]
