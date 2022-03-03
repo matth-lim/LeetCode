339ca88be0aee10d68fd12df7b82cef4599da04f
Solitude <solitude888888@gmail.com>
2022-03-03 23:44:14 +0800

    init problem 14

---
+++ 0014. Longest Common Prefix/Solution.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+
+using std::vector, std::string;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs){
+        string res;
+        for (int i = 0; i < strs[0].size(); i++)
+        {
+            for (int j = 1; j < strs.size(); j++)
+            {
+                if (strs[0][i] != strs[j][i])
+                    return res;
+            }
+            res += strs[0][i];
+        }
+        return res;
+    }
+};
