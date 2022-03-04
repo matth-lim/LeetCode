5bdfdc51e3265e2b44ea57b678d40b19dc9b55d3
Solitude <solitude888888@gmail.com>
2022-03-04 14:52:40 +0800

    init problem 21

---
+++ 0021. Merge Two Sorted Lists/README.md
@@ -0,0 +1,4 @@
+# Solution
+
+Time Complexity: **O(m + n)**\
+Space Complexity: **O(m + n)**
---
+++ 0021. Merge Two Sorted Lists/Solution.cpp
@@ -0,0 +1,36 @@
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* res = new ListNode();
+        ListNode* temp = res;
+        while (list1 && list2)
+        {
+            if (list1->val <= list2->val)
+            {
+                temp->next = new ListNode(list1->val);
+                temp = temp->next;
+                list1 = list1->next;
+            }
+            else
+            {
+                temp->next = new ListNode(list2->val);
+                temp = temp->next;
+                list2 = list2->next;
+            }
+        }
+        if (list1)
+            temp->next = list1;
+        if (list2)
+            temp->next = list2;
+        return res->next;
+    }
+};
